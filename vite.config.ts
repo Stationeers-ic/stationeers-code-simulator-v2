b0d8cab9626d4f21577d5a9215923af87c589a85
Kirill <traineratwot@yandex.ru>
2025-10-24 19:40:20 +0300

    Add PWA support and update app metadata
    Introduces Progressive Web App (PWA) support using vite-plugin-pwa, adds a new favicon.svg, and configures asset generation. Updates index.html to remove legacy favicon and manifest links, adds PWABadge component for offline/refresh notifications, and updates package.json with new dependencies and project name.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
@@ -11,5 +11,35 @@ export default defineConfig({
 			},
 		}),
 		tsconfigPaths(),
+		VitePWA({
+			registerType: "prompt",
+			injectRegister: false,
+
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
+
+			manifest: {
+				name: "Stationeers Code Simulator",
+				short_name: "Ic10",
+				description:
+					"Stationeers Code Simulator (SCS) provides a simulation of the IC10. IDE with devices, slots, networks, error checking, full visibility of stack and registers.",
+				theme_color: "#062845",
+			},
+
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+			},
+
+			devOptions: {
+				enabled: false,
+				navigateFallback: "index.html",
+				suppressWarnings: true,
+				type: "module",
+			},
+		}),
 	],
 });
