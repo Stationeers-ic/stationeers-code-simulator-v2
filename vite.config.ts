014485492ea04bfd7318fb0a482e19b5469f7d03
Kirill <traineratwot@yandex.ru>
2025-10-25 23:21:04 +0300

    Inject analytics script via Vite HTML plugin
    Commented out the analytics script in index.html and added conditional injection of the script using vite-plugin-html in vite.config.ts. The script will now only be included in production builds, improving development workflow and build flexibility.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -46,8 +47,18 @@ export default defineConfig({
 				type: "module",
 			},
 		}),
+		createHtmlPlugin({
+			minify: true,
+			inject: {
+				data: {
+					injectScript:
+						process.env.NODE_ENV === "production"
+							? '<script defer data-domain="beta.ic10.dev" src="https://thor.traineratwot.site/js/script.hash.js"></script>'
+							: "",
+				},
+			},
+		}),
 	],
-	// Добавьте эту секцию для разделения бандла
 	build: {
 		rollupOptions: {
 			output: {
